7ec332d7781a80fa8e8cbeebb2c7d23de91475dc
Fernando Boza <hi@fernandoboza.com>
2017-08-18 17:48:56 -0400

    updated resume react code

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -102,6 +102,68 @@ class Resume extends Component {
                     <Col lg='7' xs='12' className='pl-9 col-2'>
                         <h1>Fernando Boza</h1>
                         <h3>Creative Director</h3>
+
+                        <h3 className='mt-11 right-line_r'>
+                           <span>PROFILE</span>
+                        </h3>
+                        <p className='para-text text-justify'>I'm by craft a designer with over 7 years 
+                           of experience in web design, web development, 3D, and graphic design. For the 
+                           past 2 years I have organised developer communities in London, Barcelona, Munich, 
+                           Berlin, Paris, Amsterdam, and Bucharest. As well as working with clients such as Intel,
+                           Microsoft, Salesforce, McDonalds, HP, Samsung, Tesco, AB InBev, NBC Universal and many more.
+                        </p>
+
+                        <p className="para-text text-jusitfy mt-5">
+                           Literate in <b>HTML5, CSS3, jQuery, React, Angular 2, Node, Express, MongoDB, Swift and PHP.</b>
+                        </p>
+
+                        <h3 className='my-7 right-line_r'>
+                           <span>WORK EXPERIENCE</span>
+                        </h3>
+
+                        <div className="work_experience">
+                           <Row>
+                              <Col><h5>Developer Writer</h5></Col>
+                              <Col><h5>July 2016 - Feb 2017</h5></Col>
+                           </Row>
+                           <h5 className="para-text">BeMyApp</h5>
+                           <ul>
+                              <li>Write articles and tutorials revolving the developer community</li>
+                              <li>Research and write on Javascript frameworks and libraries</li>
+                           </ul>
+                        </div>
+                        
+                        <div className="work_experience">
+                           <Row>
+                              <Col><h5>Project Manager</h5></Col>
+                              <Col><h5>Nov 2014 - July 2016</h5></Col>
+                           </Row>
+                           <h5 className="para-text">BeMyApp | Paris</h5>
+                           <ul>
+                              <li>Manage developer communities in UK, Spain, Germany, and France</li>
+                              <li>Work with clients on logistics and marketing</li>
+                              <li>Responsible for outreach and social media</li>
+                              <li>Web design and development</li>
+                           </ul>
+                        </div>
+
+                        <div className="work_experience">
+                           <Row>
+                              <Col><h5>Lead Graphic Designer</h5></Col>
+                              <Col><h5>2012 - 2014</h5></Col>
+                           </Row>
+                           <h5 className="para-text">MegaTV | Miami</h5>
+                           <ul>
+                              <li>Worked with producers and director for daily graphics</li>
+                              <li>Manage design team for graphic packages</li>
+                              <li>Responsible for motion graphics and photo manipulation</li>
+                           </ul>
+                        </div>
+
+                        <h3 className='my-7 right-line_r'>
+                           <span>Education</span>
+                        </h3>
+
                     </Col>
                 </Row>
             </Container>
